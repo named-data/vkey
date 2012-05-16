4747b920bcdc0fc63c51d6269aa89c4d35deef58
Zhenkai Zhu <zhenkai@cs.ucla.edu>
2012-05-16 15:20:25 -0700

    clean

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,4 +1,5 @@
-#define BOOST_TEST_MAIN 
+#define BOOST_TEST_NO_MAIN 
+#define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
 #include <boost/test/output_test_stream.hpp>
 using boost::test_tools::output_test_stream;
@@ -14,7 +15,7 @@ BOOST_AUTO_TEST_SUITE(VKeyTests)
 
 BOOST_AUTO_TEST_CASE(Sqlite3Test)
 {
-
+	BOOST_CHECK_EQUAL(1,1);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
